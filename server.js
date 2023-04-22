8dadf52dda3b8a47387b58314939834d601e2806
Sona Punuru <punuru@email.unc.edu>
2023-04-22 04:22:12 -0400

    testing6

--- server.js
+++ server.js
@@ -2,54 +2,61 @@
 import express from 'express';
 import minimist from 'minimist';
 import { rps, rpsls } from './lib/rpsls.js'
+var argv = minimist(process.argv.slice(2));
+const port = argv.port || 5000;
 
 const app = express();
-var args = minimist(process.argv.slice(2));
-const PORT = args.port || 5000;
-
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+
 
 app.get('/app', (req, res) => {
-	res.status(200).send("200 OK");
-})
+    res.status(200).send('200 OK').end();
+});
+
 
 app.get('/app/rps', (req, res) => {
-	res.status(200).send(rps());
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
 })
+
 
 app.get('/app/rpsls', (req, res) => {
-	res.status(200).send(rpsls());
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
 })
+
 
 app.get('/app/rps/play', (req, res) => {
-	res.status(200).send(rps(req.query.shot));
+    res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
 })
+
 
 app.get('/app/rpsls/play', (req, res) => {
-	res.status(200).send(rpsls(req.query.shot));
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot))).end();
 })
+
 
 app.post('/app/rps/play', (req, res) => {
-	res.status(200).send(rps(req.body.shot));
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
 })
+
 
 app.post('/app/rpsls/play', (req, res) => {
-	res.status(200).send(rpsls(req.body.shot));
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
 })
 
-app.get('/app/rps/play/:arg', (req, res) => {
-	res.status(200).send(rps(req.params.arg));
+
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot))).end();
 })
 
-app.get('/app/rpsls/play/:arg', (req, res) => {
-	res.status(200).send(rpsls(req.params.arg));
+
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
 })
 
-app.get('*', (req, res) => {
-	res.status(404).send('404 NOT FOUND');
+
+app.all('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND').end();
 })
 
-app.listen(PORT, () => {
-	console.log(`App listening on port ${PORT}`);
-})
+app.listen(port);
